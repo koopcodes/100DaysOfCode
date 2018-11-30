0b0613e7ddfa617c773f7ae9a52487b78e224e51
koopdev <koop@koop.codes>
2018-11-29 22:21:31 -0800

    November29

--- Day11.js
+++ Day11.js
@@ -13,8 +13,12 @@
 
 function encrypt(text, n) {
 	if (n < 1) {
-    console.log(text);
+		console.log(text);
 		return text;
+	}
+	if (text === null){
+		console.log(null)
+		return null;
 	}
 	function replace() {
 		var evens = '';
@@ -35,36 +39,43 @@ function encrypt(text, n) {
 }
 
 function decrypt(encryptedText, n) {
-  if (n < 1) {
-    console.log(encryptedText);
-		return text;
-  }
-  function unreplace() {
-    if (encryptedText.length%2 ==0 ){
-      var midPoint = Math.round(encryptedText.length/2);
-      var evens = encryptedText.substr(0, midPoint);
-      var odds = encryptedText.substr(midPoint, encryptedText.length);
-    } else {
-      var midPoint = Math.round((midPoint.length/2)+1);
-      var evens = encryptedText.substr(0, midPoint);
-      var odds = encryptedText.substr(midPoint, encryptedText.length);
-    }
-    // encryptedText = evens + odds;
-		for (j = 0; j < encryptedText.length; j++) {
-			if (j % 2 === 1) {
-				odds += encryptedText[j];
-			} else evens += encryptedText[j];
-		}
-		
+	if (n < 1 ) {
+		console.log(encryptedText);
 		return encryptedText;
-  }
-  for (i = 0; i < n; i++) {
+	}
+	if (encryptedText === null){
+		console.log(null)
+		return null;
+	}
+	function unreplace() {
+		var evens = '';
+		var odds = '';
+		var text = '';
+		if (encryptedText.length % 2 == 0) {
+			var midPoint = Math.round(encryptedText.length / 2);
+			var evens = encryptedText.substr(0, midPoint);
+			var odds = encryptedText.substr(midPoint, encryptedText.length);
+		} else {
+			var midPoint = Math.round(encryptedText.length / 2 - 1);
+			var evens = encryptedText.substr(0, midPoint);
+			var odds = encryptedText.substr(midPoint, encryptedText.length);
+		}
+		for (j = 0; j < midPoint; j++) {
+			text += odds[j];
+			text += evens[j];
+		}
+		if (encryptedText.length % 2 === 1) {
+			text += odds[midPoint];
+		}
+		encryptedText = text;
+		return encryptedText;
+	}
+	for (i = 0; i < n; i++) {
 		unreplace(encryptedText);
 	}
 	console.log(encryptedText);
 	return encryptedText;
-
 }
 
-encrypt('This is a test!', 1);
-decrypt('hsi  etTi sats!', 1);
+encrypt('This is a test!', 2);
+decrypt('s eT ashi tist!', 2);
