de77b2e2299024c31460d0a5bf939f91038766d2
koopdev <koop@koop.codes>
2018-12-01 19:00:48 -0800

    December1

--- Day14.js
+++ Day14.js
@@ -1,22 +1,27 @@
+// Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+
 function persistence(num) {
+	var iterations = 0;
 	var length = num.toString().length;
 	var digits = num
 		.toString(10)
 		.split('')
-		.map(Number);
-
-	function multiply(digits) {
-		var iterations = 0;
+    .map(Number);
+    
+	multiply(digits, iterations);
+  
+  function multiply(digits, iterations) {
+		iterations = iterations + 1;
 		var sum = 1;
 		for (i = 0; i < length; i++) {
-      iterations ++;
 			sum = sum * digits[i];
+			console.log('Sum: ', sum);
 		}
-
 		if (length === 1) {
+      console.log(iterations);
 			return iterations;
 		} else {
-			multiply(sum);
+			multiply(sum, iterations);
 		}
 	}
 }
