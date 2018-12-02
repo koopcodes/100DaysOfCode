4a4ac45bebcd314f0ad4d41f7f15be16e7690429
koopdev <koop@koop.codes>
2018-12-02 14:57:28 -0800

    December2

---
+++ Day16.js
@@ -0,0 +1,8 @@
+// Tribonacci
+// Like a Fibonacci, but summing the last 3 (instead of 2) numbers 
+// Create a fibonacci function that given a signature array/list, returns the first n elements - signature included of the so seeded sequence
+// Signature will always contain 3 numbers; n will always be a non-negative number; if n == 0, then return an empty array and be ready for anything else which is not clearly specified ;
+
+function tribonacci(signature,n){
+  //your code here
+}
