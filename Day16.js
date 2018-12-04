1236dea8e25277ef592fe6dc8ddbb4a9e15e0f38
koopdev <koop@koop.codes>
2018-12-03 19:10:35 -0800

    December3

--- Day16.js
+++ Day16.js
@@ -7,29 +7,24 @@ function tribonacci(signature, n) {
 	if (n === 0 || isNaN(n)) {
 		console.log([]);
 		return [];
-  } 
-  // else if (n === 1) {
-  //   return [1];
-  // }
-   else {
-		for (i = 0; signature.length < n; i++) {
+	} else {
+		for (i = 0; signature.length < n; i++) { // iterate n times
 			sum = signature
-				.slice(Math.max(signature.length - 3, 0))
+				.slice(Math.max(signature.length - 3, 0))   // add last 3 array items and push to trib
 				.reduce((a, b) => a + b, 0);
 			signature.push(sum);
 		}
-  }
-  return signature.slice(0,n);
+	}
+	return signature.slice(0, n);  //return trib - length of n
 }
 
 tribonacci([1, 1, 1], 10);
 // [1,1,1,3,5,9,17,31,57,105]
 
-
 ///// OR /////
-function tribonacci2(signature,n){  
-  for (var i = 0; i < n-3; i++) { // iterate n times
-    signature.push(signature[i] + signature[i+1] + signature[i+2]); // add last 3 array items and push to trib
-  }
-  return signature.slice(0, n); //return trib - length of n
+function tribonacci2(signature, n) {
+	for (var i = 0; i < n - 3; i++) { // iterate n times
+		signature.push(signature[i] + signature[i + 1] + signature[i + 2]); // add last 3 array items and push to trib
+	}
+	return signature.slice(0, n); //return trib - length of n
 }
