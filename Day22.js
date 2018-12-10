e0a1a470ccc8fd901e9c8f8624a996b6b749ac05
koopdev <koop@koop.codes>
2018-12-09 18:16:11 -0800

    December9

---
+++ Day22.js
@@ -0,0 +1,17 @@
+// There is an array with some numbers. All numbers are equal except for one. Try to find it!
+// findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+// findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+// It’s guaranteed that array contains more than 3 numbers
+
+function findUniq(arr) {
+  console.log(arr);
+  console.log(arr.filter(function(value){
+    return arr.indexOf(value) === arr.lastIndexOf(value);
+  })[0] || -1);
+  
+  return arr.filter(function(value){
+    return arr.indexOf(value) === arr.lastIndexOf(value);
+  })[0] || -1;
+}
+
+findUniq([ 0, 1, 1, 1, 1, 1, 1, 1 ]); // 10
