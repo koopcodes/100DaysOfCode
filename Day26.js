adac6fc97a00cb76f5a70a8a1339bd22d33685c2
koopdev <koop@koop.codes>
2018-12-12 20:12:33 -0800

    December 12

---
+++ Day26.js
@@ -0,0 +1,68 @@
+// The rgb() method is incomplete. Complete the method so that passing in RGB decimal values will result in a hexadecimal representation being returned. The valid decimal values for RGB are 0 - 255. Any (r,g,b) argument values that fall out of that range should be rounded to the closest valid value.
+// The following are examples of expected output values:
+// rgb(255, 255, 255) // returns FFFFFF
+// rgb(255, 255, 300) // returns FFFFFF
+// rgb(0,0,0) // returns 000000
+// rgb(148, 0, 211) // returns 9400D3
+
+function rgb(r, g, b) {
+	const rHex = decimalToHex(clamp(r));
+	const gHex = decimalToHex(clamp(g));
+	const bHex = decimalToHex(clamp(b));
+	console.log(rHex + gHex + bHex);
+	return rHex + gHex + bHex;
+}
+
+function decimalToHex(decimal) {
+	return (decimal + 0x10000)
+		.toString(16)
+		.substr(-2)
+		.toUpperCase();
+}
+
+function clamp(decimal) {
+	return Math.min(255, Math.max(0, decimal));
+}
+
+rgb(255, 255, 300);
+
+///////////////////////////
+// Divisors of 42 are : 1, 2, 3, 6, 7, 14, 21, 42. These divisors squared are: 1, 4, 9, 36, 49, 196, 441, 1764. The sum of the squared divisors is 2500 which is 50 * 50, a square!
+// Given two integers m, n (1 <= m <= n) we want to find all integers between m and n whose sum of squared divisors is itself a square. 42 is such a number.
+// The result will be an array of arrays or of tuples (in C an array of Pair) or a string, each subarray having two elements, first the number whose squared divisors is a square and then the sum of the squared divisors.
+// #Examples:
+// list_squared(1, 250) --> [[1, 1], [42, 2500], [246, 84100]]
+// list_squared(42, 250) --> [[42, 2500], [246, 84100]]
+
+function listSquared(m, n) {
+	var result = [];
+	for (i = m; i <= n; i++) {
+		var sum = 0;
+		var squareFactors = getFactors(i);
+		for (j = 0; j < squareFactors.length; j++) {
+			squareFactors[j] = Math.pow(squareFactors[j], 2);
+		}
+		sum = squareFactors.reduce((a, b) => a+b, 0);
+		if (Math.sqrt(sum) % 1 === 0) {
+			result.push([i, sum]);
+		}
+	}
+
+  console.log(result);
+  return result;
+
+	function getFactors(integer) {
+		var factors = [];
+		var quotient = 0;
+		for (var i = 1; i <= integer; i++) {
+			quotient = integer / i;
+
+			if (quotient === Math.floor(quotient)) {
+				factors.push(i);
+			}
+		}
+		return factors;
+	}
+}
+
+listSquared(1, 250);
