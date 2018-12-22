78a5418b8856ae23d03968004388f41806543e7e
koopdev <koop@koop.codes>
2018-12-21 21:03:27 -0800

    December21

--- Day28.js
+++ Day28.js
@@ -8,4 +8,41 @@ function pigIt (str){
   return str.replace(/\b(\w)(\w+)\b/g, '$2$1ay');
 };
 
-pigIt('O tempora o mores !');
+////////
+function pigIt2(str){
+  if(typeof(str)!=='string'){
+    console.log('please you must enter a valid string. The value you entered is not a string');
+    return false;
+  }
+  //convert the sentence to lowercase letters and split the sentence into an array of words
+  var words = str.toLowerCase().split(" ");
+  
+  //create an array to hold the new pigLatin sentence
+  var pigLatinSentence = [];
+  
+  //loop through each word in the new array and split it to an array of characters
+  for(var i=0;i<words.length;i++){
+    letters = words[i].split("");
+    console.log(letters);
+    //extract the first element of the array, concantenate with "ay" and push it to the array of letters
+    if (letters != '!') {
+    letters.push(letters.splice(0,1)+"ay");
+    } else letters.push(letters.splice(0, 1));
+    //join the letters to form a new word
+    word = letters.join('');
+    
+    //if it is the first word of the sentence, convert the first letter to UpperCase
+    if(i===0){
+      word = word.slice(0,1).toUpperCase() + word.slice(1);
+    }
+    
+    //push the newly formed words to the pigLatinSentence array
+    pigLatinSentence.push(word);
+  }
+  
+  //join the words to form a new sentence
+  pigLatinSentence = pigLatinSentence.join(" ");
+  console.log(pigLatinSentence);
+  return pigLatinSentence;
+}
+pigIt2('O tempora o mores !');
