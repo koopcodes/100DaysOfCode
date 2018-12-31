86a37f7a6cbb2b9fb8c0ffe49c25945bf4aaa36d
koopdev <koop@koop.codes>
2018-12-30 21:09:16 -0800

    December30

--- Day33.js
+++ Day33.js
@@ -6,8 +6,8 @@ function productFib(prod) {
 	while (n * nPlus < prod) {
 		nPlus = n + nPlus;
 		n = nPlus - n;
-  }
-  console.log([n, nPlus, n*nPlus===prod]);
+	}
+	console.log([n, nPlus, n * nPlus === prod]);
 	return [n, nPlus, n * nPlus === prod];
 }
 
@@ -15,16 +15,36 @@ productFib(4895); // [ 55, 89, true ]
 productFib(5895); // [ 89, 144, false ]
 
 ///// OR /////
-function productFib2( prod ) // mathy way
-{
-  const r = ( 1 + Math.sqrt(5) ) / 2;
-  let fib1 = Math.round( Math.sqrt( prod / r ) ),
-  fib2 = Math.round( fib1 * r );
-  if ( fib1 * fib2 === prod ) return [ fib1, fib2, true ];
-  for ( [fib1, fib2] = [0, 1]; fib1 * fib2 < prod; [fib1, fib2] = [fib2, fib1 + fib2] );
-  console.log([ fib1, fib2, false ]);
-  return [ fib1, fib2, false ];
+function productFib2(prod) {
+	// mathy way
+	const r = (1 + Math.sqrt(5)) / 2;
+	let fib1 = Math.round(Math.sqrt(prod / r)),
+		fib2 = Math.round(fib1 * r);
+	if (fib1 * fib2 === prod) return [fib1, fib2, true];
+	for (
+		[fib1, fib2] = [0, 1];
+		fib1 * fib2 < prod;
+		[fib1, fib2] = [fib2, fib1 + fib2]
+	);
+	console.log([fib1, fib2, false]);
+	return [fib1, fib2, false];
 }
 
 productFib2(4895); // [ 55, 89, true ]
 productFib2(5895); // [ 89, 144, false ]
+
+////////////////////
+//Complete the function/method (depending on the language) to return true/True when its argument is an array that has the same nesting structure as the first array
+// Return 'true' if and only if 'other' has the same
+// nesting structure as 'this'.
+// Note: You are given a function isArray(o) that returns
+// whether its argument is an array.
+Array.prototype.sameStructureAs = function(other) {
+	return this.length === other.length
+		? this.every(function(val, ind) {
+				return Array.isArray(val) ? val.sameStructureAs(other[ind]) : true;
+		  })
+		: false;
+};
+
+console.log([ 1, 1, 1 ].sameStructureAs( [ 2, 2, 2 ] )); 
