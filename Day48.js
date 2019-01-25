e61e2c65fbf2124e74fe5d952525c0ca8cf01136
koopdev <koop@koop.codes>
2019-01-24 20:46:56 -0800

    January24

--- Day48.js
+++ Day48.js
@@ -16,6 +16,11 @@ console.log(domainName("http://google.co.jp")); // google.co.jp
 
 function domainName2(url) {
 	var urlPattern = /^(?:https?:\/\/)?(?:w{3}\.)?([a-z\d\.-]+)\.(?:[a-z\.]{2,10})(?:[/\w\.-]*)*/;
+	// ^(?:https?:\/\/)? ---> is there an http or https part? don't capture them.
+	// (?:w{3}.)? ---> are there 3 w? don't capture them
+	// ([a-z\d.-]+). ---> the domain name part. Capture it.
+	// (?:[a-z.]{2,10})(?:[/\w.-]) ---> the directories and folders part. don't capture them.
+
 	var domainPattern = url.match(urlPattern);
 	var extractDomain = domainPattern[1];
 	return extractDomain;
@@ -23,3 +28,15 @@ function domainName2(url) {
 
 console.log(domainName2("http://google.com"));  // google
 console.log(domainName2("http://google.co.jp")); // google.co
+
+function domainName3(url) {
+	var urlPattern = /^(?:https?:\/\/)?(?:w{3}\.)?([a-z\d\-]+)\.(?:[a-z\.]{2,10})(?:[/\w\.-]*)*/;
+	// removed the . after \d\ and before minus. Was ([a-z\d\.-]+)
+
+	var domainPattern = url.match(urlPattern);
+	var extractDomain = domainPattern[1];
+	return extractDomain;
+}
+
+console.log(domainName3("http://google.com"));  // google
+console.log(domainName3("http://google.co.jp")); // google
