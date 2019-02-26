dcb1fce5424f8e318d4dc636686ca1dd689a2c00
koopdev <koop@koop.codes>
2019-02-25 22:04:52 -0800

    Clean Up

--- Day52.js
+++ Day52.js
@@ -20,4 +20,22 @@ function intersection(list1, list2) {
 function intersection2(list1, list2) {
 	return list1.filter((value) => list2.indexOf(value) !== -1);
 }
-console.log(intersection2([1, 2, 2, 3], [2, 2, 3, 4]));
+// console.log(intersection2([1, 2, 2, 3], [2, 2, 3, 4]));
+
+function intersection3(list1, list2) {
+	var countsObj = {};
+	var results = [];
+	for (let i = 0; i < list1.length; i++) {
+		!countsObj[list1[i]] ? countsObj[list1[i]] = 1 : countsObj[list1[i]]++;
+	}
+	for (let j = 0; j < list2.length; j++) {
+		if (!countsObj[list2[j]]) {
+			break;
+		} else {
+			results.push(list2[j]);
+			countsObj[list1[j]]--;
+		}
+	}
+	return results;
+}
+console.log(intersection3([1, 2, 2, 3], [2, 2, 3, 4]));
