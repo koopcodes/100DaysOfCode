8560bc13457ddc5c56f234cc095efe7f9736cb6c
koopdev <koop@koop.codes>
2019-03-14 12:50:20 -0700

    March13

--- Day56.js
+++ Day56.js
@@ -2,6 +2,7 @@ var matrix = [[1, 2, 3, 4],
 							[5, 6, 7, 8],
 							[9, 10, 11, 12],
 							[13, 14, 15, 16]];
+var result = [];
 
 // https://medium.com/front-end-weekly/matrix-rotation-%EF%B8%8F-6550397f16ab
 const flipMatrix = matrix => (
@@ -10,12 +11,24 @@ const flipMatrix = matrix => (
 	))
 );
 
+// Reversing the matrix before the flip will yield a clockwise rotation.
+// Reversing the matrix after the flip will yield a counter-clockwise rotation.
+
 const rotateClockwise = matrix => (
 	flipMatrix(matrix.reverse())			// reverse the matrix before flipping it
 );
 
 const rotateCounterClockwise = matrix => (
-	flipMatrix(matrix).reverse()			// flip the matrix before revesing it
+	flipMatrix(matrix).reverse()			// flip the matrix before reversing it
 );
+console.log(matrix.length);
+function traverseSpiral() {
+	console.log(matrix.length);
+	if (matrix.length != 0) {
+		result = result.push(matrix[0]);
+		matrix = matrix[0].pop();
+		traverseSpiral()
+	} else return result;
+}
 
-console.log(rotateCounterClockwise(matrix));
+traverseSpiral();
