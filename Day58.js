750c1afe48c4661f7e3654c71ddeeae4f8e45109
koopdev <koop@koop.codes>
2019-03-18 19:28:00 -0700

    March18

--- Day58.js
+++ Day58.js
@@ -6,34 +6,45 @@ var matrix = [[1, 2, 3, 4],
 [13, 14, 15, 16]];
 var result = [];
 
-const flipMatrix = matrix => (
+const flipMatrix = () => (
+
 	matrix[0].map((column, index) => (
-		matrix.map(row => row[index])		// this map extracts a single column from a matrix, converting it into a row.
-	))
+		matrix.map(row => row[index])
+		// this map extracts a single column from a matrix, converting it into a row.
+	)),
+	console.log('flip: ', matrix)
 );
 
-const rotateClockwise = matrix => (
-	flipMatrix(matrix.reverse())			// reverse the matrix before flipping it
+const rotateClockwise = () => (
+	flipMatrix(matrix.reverse())
+	// reverse the matrix before flipping it
 );
 
-const rotateCounterClockwise = matrix => (
-	flipMatrix(matrix).reverse()			// flip the matrix before reversing it
+const rotateCounterClockwise = () => (
+	console.log(Object.prototype.toString.call(matrix)),
+	flipMatrix(matrix).reverse(),
+	console.log('rotate: ', matrix)
+	// flip the matrix before reversing it
 );
 
-const readLine = result => (
-	result.push(matrix[0])
+const readLine = () => (
+	result.push(matrix[0]),
+	console.log('read: ', result)
 );
 
-const delLine = matrix => (
-	matrix.splice(0, 1)
+const delLine = () => (
+	matrix.splice(0, 1),
+	console.log('delete: ', matrix)
 );
 
 function traverseSpiral() {
-	// console.log(Object.prototype.toString.call(matrix));
-	console.log('matrix.length: ', matrix.length);
+	
 	if (matrix.length > 0) {
+		console.log(Object.prototype.toString.call(matrix));
+		console.log('matrix.length: ', matrix.length);
 		readLine();
 		delLine();
+		console.log(rotateCounterClockwise());
 		rotateCounterClockwise();
 		traverseSpiral();
 	} else {
